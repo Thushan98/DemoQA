7d37032ff442a1c46f4fb0a45eac122866a5f6cd
Thushan98 <thushandananjanakk@gmail.com>
2024-04-11 23:05:14 +0530

    text box testing done

--- src/test/java/elements/ElementsSetup.java
+++ src/test/java/elements/ElementsSetup.java
@@ -1,11 +1,13 @@
 package elements;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeTest;
 import setup.SetupProject;
 
-public class Elements {
+public class ElementsSetup {
     SetupProject setup = new SetupProject();
     WebDriver driver = setup.settingDriver();
 
@@ -13,6 +15,8 @@ public class Elements {
     public WebDriver settingPage() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,350)", "");
+        WebElement elementText = driver.findElement(By.xpath("//h5[text()='Elements']"));
+        elementText.click();
         return driver;
     }
 }
--- src/test/java/elements/TextBoxes.java
+++ src/test/java/elements/TextBoxes.java
@@ -1,18 +1,44 @@
 package elements;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class TextBoxes {
-    Elements setup = new Elements();
+    ElementsSetup setup = new ElementsSetup();
     WebDriver driver = setup.settingPage();
 
 
     @Test
+    @Deprecated
     public void testTextBoxes() {
-        WebElement elementText = driver.findElement(By.xpath("//h5[text()='Elements']"));
-        elementText.click();
+        WebElement textBoxText = driver.findElement(By.id("item-0"));
+        textBoxText.click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,350)", "");
+
+        WebElement userName = driver.findElement(By.id("userName"));
+        userName.sendKeys("thushanLogan");
+
+        WebElement email = driver.findElement(By.id("userEmail"));
+        email.sendKeys("thushan@gmail.com");
+
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        currentAddress.sendKeys("No.70/A, BoxOffice Road, Daka");
+
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+        permanentAddress.sendKeys("same as above");
+
+        WebElement submit = driver.findElement(By.id("submit"));
+        userName.click();
+
+        js.executeScript("window.scrollBy(0,750)", "");
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        driver.quit();
     }
 }
